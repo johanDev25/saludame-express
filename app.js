06a3ceea0f4795bec42eb6b594413fbfd8427259
Johan <johan225@hotmail.es>
2019-11-29 14:35:53 -0500

    Agrega h1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send(req.query.nombre ? "Hola " + req.query.nombre + " !" : " Hola desconocido "+ "!");
+  res.send(req.query.nombre ? "<h1>Hola " + req.query.nombre + " !</h1>" : "<h1>Hola desconocido !</h1>");
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
